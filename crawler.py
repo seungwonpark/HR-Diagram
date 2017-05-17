fb2337806affe7f8dd7349db104f72d1b774a887
Seungwon Park <psw14041@gmail.com>
2017-05-17 16:31:39 +0900

    Implement grid search - explorer.py

--- crawler.py
+++ crawler.py
@@ -3,15 +3,18 @@ import re
 
 baseurl = 'http://skyserver.sdss.org/dr7/sp/tools/chart/shownearest.asp'
 # '?ra=114.59053567107003&dec=21.56293906666667&scale=0.39612&opt=&radius=0.2'
+
 def coordinates_str(ra, dec, scale):
 	return '?ra=%s&dec=%s&scale=%s' % (str(ra), str(dec), str(scale))
 
+def crawl_url(ra, dec, scale):
+	url = baseurl + coordinates_str(ra, dec, scale)
+	f = urllib.request.urlopen(url)
+	html = f.read().decode('utf-8')
 
-url = baseurl + coordinates_str(114.5905, 21.563, 0.5)
-
-f = urllib.request.urlopen(url)
-# print(f.read())
-
-html = f.read().decode('utf-8')
-m = re.findall(r"<td align='right' class='c'>(.*)<\/td>", html)
-print(m)
+	try:
+		objectNo = re.findall(r"javascript:explore\('(.*)'\)", html)[0]
+		m = re.findall(r"<td align='right' class='c'>(.*)<\/td>", html)
+		return [objectNo] + m
+	except:
+		return 0
---
+++ explorer.py
@@ -0,0 +1,33 @@
+from crawler import crawl_url
+
+ra_0 = 114.543
+dec_0 = 21.641
+ra_1 = 114.665
+dec_1 = 21.526
+
+#interval = 0.001
+interval = 0.02
+
+
+ra_0, ra_1 = sorted([ra_0, ra_1])
+dec_0, dec_1 = sorted([dec_0, dec_1])
+
+ra_length = int((ra_1 - ra_0) // interval)
+dec_length = int((dec_1 - dec_0) // interval)
+
+filename = 'result/%g-%g-%g-%g-%g.csv' % (ra_0, ra_1, dec_0, dec_1, interval)
+
+f = open(filename, 'w')
+f.write('ObjectNo, ra, dec, type, u, g, r, i, z\n')
+
+for ra_i in range(ra_length):
+	for dec_i in range(dec_length):
+		ra = ra_0 + interval * ra_i
+		dec = dec_0 + interval * dec_i
+		print(ra, dec)
+
+		data = crawl_url(ra, dec, 0.5)
+		if(data == 0):
+			continue
+		else:
+			f.write(", ".join(data) + "\n")
