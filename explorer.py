8cf11ffa10d041ff5f75ba96a631a4b6191a92a4
Seungwon Park <psw14041@gmail.com>
2017-05-17 17:43:19 +0900

    Remove threshold from explorer.py

--- explorer.py
+++ explorer.py
@@ -1,7 +1,5 @@
 from crawler import crawl_url
 
-threshold = 99 # if object's "r" is darker than threshold, then skip.
-
 def grid_search(ra_0, ra_1, dec_0, dec_1, interval):
 	ra_0, ra_1 = sorted([ra_0, ra_1])
 	dec_0, dec_1 = sorted([dec_0, dec_1])
@@ -9,7 +7,7 @@ def grid_search(ra_0, ra_1, dec_0, dec_1, interval):
 	ra_length = int((ra_1 - ra_0) // interval)
 	dec_length = int((dec_1 - dec_0) // interval)
 
-	datafilename = 'data/%g-%g-%g-%g-%g-%g.csv' % (ra_0, ra_1, dec_0, dec_1, interval, threshold)
+	datafilename = 'data/%g-%g-%g-%g-%g.csv' % (ra_0, ra_1, dec_0, dec_1, interval)
 
 	f = open(datafilename, 'w')
 	f.write('ObjectNo, ra, dec, type, u, g, r, i, z\n')
@@ -26,7 +24,7 @@ def grid_search(ra_0, ra_1, dec_0, dec_1, interval):
 			if(data == 0):
 				continue
 			else:
-				if(float(data[-3]) < threshold and data[3] == 'STAR'):
+				if(data[3] == 'STAR'):
 					if(data[0] not in ObjectList): # to avoid duplication
 						ObjectList.append(data[0])
 						f.write(", ".join(data) + "\n")
